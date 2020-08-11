4c13fa1fc9285cb76510591c709e48e55e3196f2
Demian Brecht <demianbrecht@gmail.com>
2020-08-11 10:45:55 -0700

    update version to 0.22.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,7 +73,7 @@ author = 'Salesforce'
 # built documents.
 
 # The full version, including alpha/beta/rc tags.
-release = '0.22.0'  # set by bumpversion
+release = '0.22.1'  # set by bumpversion
 
 # The short X.Y version.
 version = release.rsplit('.', 1)[0]
