ba697a816711ee17731a40cf7c3ea91a21dcfd0b
Hila Manalai <hmanalai@hmanala-ltmhtya.internal.salesforce.com>
2021-05-26 16:13:47 -0400

    added testutils for testing me endpoint

--- example/myapp/tests/test_responses.py
+++ example/myapp/tests/test_responses.py
@@ -3,10 +3,8 @@ import http
 import oauthlib
 
 from django.test import TestCase
-from django.http import QueryDict, HttpRequest
-from oauthlib.oauth1 import SIGNATURE_TYPE_QUERY
-
-
+from .testutils import  OAuthClient
+from django_declarative_apis.models import OauthConsumer
 
 class ResponseTestCase(TestCase):
     def test_ping_definition(self):
@@ -17,22 +15,12 @@ class ResponseTestCase(TestCase):
     
 
     def test_create_me(self):
-        client = oauthlib.oauth1.Client('client_key', signature_type=SIGNATURE_TYPE_QUERY,)
-
-        uri, headers, body = client.sign('http://localhost:8000/me?q=hello')
-        parameters = QueryDict(uri)
+        self.client = OAuthClient()
+        consumer = OauthConsumer.objects.create(name='smith')
 
         resp = self.client.post(
             '/me',
-            {
-                'oauth_consumer_key': parameters.get('oauth_consumer_key'),
-                'oauth_token': parameters.get('oauth_token'),
-                'oauth_signature_method': parameters.get('oauth_signature_method'),
-                'oauth_timestamp': parameters.get('oauth_timestamp'),
-                'oauth_nonce': parameters.get('oauth_nonce'),
-                'oauth_signature': parameters.get('oauth_signature'),
-            },
-            content_type='application/json'
+            {},
+            consumer=consumer,
         )
-        print("resp: ", resp.json())
         self.assertEqual(resp.status_code, http.HTTPStatus.OK)
