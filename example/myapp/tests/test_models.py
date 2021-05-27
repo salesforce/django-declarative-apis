7dd1e535156ef4acdaaf8234924a197c09f10cad
Hila Manalai <hmanalai@hmanala-ltmhtya.internal.salesforce.com>
2021-05-27 14:06:28 -0400

    run make format

--- example/myapp/tests/test_models.py
+++ example/myapp/tests/test_models.py
@@ -3,11 +3,12 @@ from django_declarative_apis import models
 
 from myapp.models import User
 
+
 class ModelsTestCase(TestCase):
     def test_create_user(self):
-        consumer = models.OauthConsumer.objects.create(name='smith')
+        consumer = models.OauthConsumer.objects.create(name="smith")
 
-        user = User(consumer=consumer, name='smith')
+        user = User(consumer=consumer, name="smith")
         user.save()
 
         self.assertEqual(user.consumer.content_type_id, consumer.content_type_id)
@@ -17,5 +18,4 @@ class ModelsTestCase(TestCase):
         self.assertEqual(user.consumer.object_id, consumer.object_id)
         self.assertEqual(user.consumer.secret, consumer.secret)
         self.assertEqual(user.consumer.type, consumer.type)
-        self.assertEqual(user.name, 'smith')
-
+        self.assertEqual(user.name, "smith")
--- example/myapp/tests/testutils.py
+++ example/myapp/tests/testutils.py
@@ -13,7 +13,7 @@ class OAuthClientHandler(ClientHandler):
         super(OAuthClientHandler, self).__init__(*args, **kwargs)
 
     def get_response(self, request):
-        consumer = OauthConsumer.objects.get(name='smith')
+        consumer = OauthConsumer.objects.get(name="smith")
 
         # Make request params mutable so we can add authorization parameters.
         # We make the params immutable again before processing the request
@@ -21,63 +21,72 @@ class OAuthClientHandler(ClientHandler):
         request.GET = request.GET.copy()
 
         if consumer:
-            if request.method == 'POST':
+            if request.method == "POST":
                 data = request.POST
             else:
                 data = request.GET
 
             # This provides a way for us to override default values for testing.
-            oauth_version = request.META.get('oauth_version', '1.0')
-            oauth_nonce = request.META.get('oauth_nonce', oauth2.generate_nonce())
-            oauth_client_timestamp = request.META.get('oauth_timestamp', int(time.time()))
+            oauth_version = request.META.get("oauth_version", "1.0")
+            oauth_nonce = request.META.get("oauth_nonce", oauth2.generate_nonce())
+            oauth_client_timestamp = request.META.get(
+                "oauth_timestamp", int(time.time())
+            )
 
-            rsa_key = request.META.get('rsa_key', None)
+            rsa_key = request.META.get("rsa_key", None)
             oauth_signature_method = oauthlib.oauth1.SIGNATURE_HMAC
 
-
             oauth_signature_data = {
-                'oauth_version': oauth_version,
-                'oauth_nonce': oauth_nonce,
-                'oauth_timestamp': str(oauth_client_timestamp),
-                'oauth_consumer_key': consumer.key,
-                'oauth_signature_method': oauth_signature_method
+                "oauth_version": oauth_version,
+                "oauth_nonce": oauth_nonce,
+                "oauth_timestamp": str(oauth_client_timestamp),
+                "oauth_consumer_key": consumer.key,
+                "oauth_signature_method": oauth_signature_method,
             }
 
             # collect ALL request parameters (original + OAuth) for signing
             all_request_parameters = data.copy()
             all_request_parameters.update(oauth_signature_data)
 
-
             # use HMAC-SHA1 signature method
-            oauth_signature_data.update({'oauth_signature_method': 'HMAC-SHA1'})
+            oauth_signature_data.update({"oauth_signature_method": "HMAC-SHA1"})
 
             # Create oauth request object to compute signature
-            oauth_request = oauth2.Request.from_consumer_and_token(consumer, None,
-                                                                   request.method,
-                                                                   request.build_absolute_uri(request.path),
-                                                                   all_request_parameters,
-                                                                   is_form_encoded=True)
+            oauth_request = oauth2.Request.from_consumer_and_token(
+                consumer,
+                None,
+                request.method,
+                request.build_absolute_uri(request.path),
+                all_request_parameters,
+                is_form_encoded=True,
+            )
 
             # Add signature to django request
             signature_method = oauth2.SignatureMethod_HMAC_SHA1()
             oauth_request.sign_request(signature_method, consumer, None)
-            oauth_signature_data['oauth_signature'] = oauth_request.get_parameter('oauth_signature').decode('utf-8')
+            oauth_signature_data["oauth_signature"] = oauth_request.get_parameter(
+                "oauth_signature"
+            ).decode("utf-8")
 
-            use_auth_header_signature = request.META.pop('use_auth_header_signature', False)
+            use_auth_header_signature = request.META.pop(
+                "use_auth_header_signature", False
+            )
             if use_auth_header_signature:
-                auth_header_string = "OAuth " + ','.join(
-                    ['{0}="{1}"'.format(key, value) for key, value in oauth_signature_data.items()]
+                auth_header_string = "OAuth " + ",".join(
+                    [
+                        '{0}="{1}"'.format(key, value)
+                        for key, value in oauth_signature_data.items()
+                    ]
                 )
-                request.META['HTTP_AUTHORIZATION'] = auth_header_string
+                request.META["HTTP_AUTHORIZATION"] = auth_header_string
             else:
                 data.update(oauth_signature_data)
 
         # Recreate the GET and POST QueryDicts to make them immutable, as in production
-        request.POST = QueryDict(request.POST.urlencode().encode('utf-8'))
-        request.GET = QueryDict(request.GET.urlencode().encode('utf-8'))
+        request.POST = QueryDict(request.POST.urlencode().encode("utf-8"))
+        request.GET = QueryDict(request.GET.urlencode().encode("utf-8"))
 
         return ClientHandler.get_response(self, request)
-
 
 
 class OAuthClient(test.Client):
