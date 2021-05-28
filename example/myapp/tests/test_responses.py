bd1ead735390226a8369c12572d03ee268d2d10e
Hila Manalai <hmanalai@hmanala-ltmhtya.internal.salesforce.com>
2021-05-28 17:40:28 -0400

    reformatted  two files

--- example/myapp/tests/test_responses.py
+++ example/myapp/tests/test_responses.py
@@ -7,21 +7,14 @@ from django_declarative_apis.models import OauthConsumer
 
 class ResponseTestCase(TestCase):
     def test_ping_definition(self):
-        resp = self.client.get(
-            "/ping",
-        )
+        resp = self.client.get("/ping")
         self.assertEqual(resp.json(), {"ping": "pong"})
 
     def test_create_me(self):
         self.client = OAuthClient()
         consumer = OauthConsumer.objects.create(name="smith")
 
-        resp = self.client.post(
-            "/me",
-            {},
-            consumer=consumer,
-            secure=True,
-        )
+        resp = self.client.post("/me", {}, consumer=consumer, secure=True)
         self.assertEqual(resp.status_code, http.HTTPStatus.OK)
         self.assertEqual(resp.json()["key"], consumer.key)
         self.assertEqual(resp.json()["name"], consumer.name)
