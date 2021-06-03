1c983b9e2b67d6c91eacc3f0eb70f307bc859a02
Hila Manalai <hmanalai@salesforce.com>
2021-06-03 09:25:15 -0400

    checked format

--- example/myapp/migrations/0001_initial.py
+++ example/myapp/migrations/0001_initial.py
@@ -8,17 +8,29 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('django_declarative_apis', '0002_add_consumer_type_field'),
-    ]
+    dependencies = [("django_declarative_apis", "0002_add_consumer_type_field")]
 
     operations = [
         migrations.CreateModel(
-            name='User',
+            name="User",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=50)),
-                ('consumer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='django_declarative_apis.OauthConsumer')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=50)),
+                (
+                    "consumer",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="django_declarative_apis.OauthConsumer",
+                    ),
+                ),
             ],
-        ),
+        )
     ]
