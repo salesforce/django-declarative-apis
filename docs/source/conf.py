37b281e39b36024aed56ea3eb59424fb87105b0d
Robert David Grant <robert.grant@salesforce.com>
2022-05-18 10:03:06 -0400

    Bump patch version (#99)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,7 +74,7 @@ author = 'Salesforce'
 # built documents.
 
 # The full version, including alpha/beta/rc tags.
-release = '0.23.0'  # set by bumpversion
+release = '0.23.1'  # set by bumpversion
 
 # The short X.Y version.
 version = release.rsplit('.', 1)[0]
