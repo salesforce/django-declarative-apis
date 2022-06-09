4e3f04e8e0ed37ead8ad3ea19b59bc59bf83cc22
Robert David Grant <robert.grant@salesforce.com>
2022-06-09 17:01:00 -0400

    Expand testing

--- tests/machinery/test_errors.py
+++ tests/machinery/test_errors.py
@@ -2,23 +2,24 @@ import django.test
 
 from django_declarative_apis.machinery import errors
 
-class ApiErrorTestCase(django.test.TestCase):
 
-    def test_tuple(self):
+class ErrorTestCast(django.test.TestCase):
+
+    def test_apierror_tuple(self):
         test_code, test_message = test_tuple = errors.HTTPS_REQUIRED
         err = errors.ApiError(error_tuple=test_tuple)
-        self.assertEqual(err.error_code, test_code)
-        self.assertEqual(err.error_message, test_message)
+        self.assertEqual(test_code, err.error_code)
+        self.assertEqual(test_message, err.error_message)
         self.assertDictEqual(err.as_dict(), dict(error_code=test_code, error_message=test_message))
 
-    def test_code_and_message(self):
+    def test_apierror_code_and_message(self):
         test_code, test_message = 600, "test message"
         err = errors.ApiError(code=test_code, message=test_message)
-        self.assertEqual(err.error_code, test_code)
-        self.assertEqual(err.error_message, test_message)
-        self.assertDictEqual(err.as_dict(), dict(error_code=test_code, error_message=test_message))
+        self.assertEqual(test_code, err.error_code)
+        self.assertEqual(test_message, err.error_message)
+        self.assertDictEqual(dict(error_code=test_code, error_message=test_message), err.as_dict())
 
-    def test_bad_args(self):
+    def test_apierror_bad_args(self):
         try:
             errors.ApiError()
         except errors.ApiError:
@@ -29,8 +30,19 @@ class ApiErrorTestCase(django.test.TestCase):
         else:
             self.fail("ApiError without arguments should raise an exception.")
 
-    def test_extra_args(self):
+    def test_apierror_extra_args(self):
         test_code, test_message = test_tuple = errors.AUTHORIZATION_FAILURE
         err = errors.ApiError(error_tuple=test_tuple, foo="bar", baz="quux")
-        self.assertDictEqual(err.as_dict(),
-                             dict(error_code=test_code, error_message=test_message, foo="bar", baz="quux"))
+        self.assertDictEqual(dict(error_code=test_code, error_message=test_message, foo="bar", baz="quux"),
+                             err.as_dict())
+
+    def test_additional_info(self):
+        test_classes = [errors.ClientErrorUnprocessableEntity, errors.ClientErrorNotFound, errors.ClientErrorForbidden,
+                        errors.ClientErrorUnauthorized, errors.ClientErrorExternalServiceFailure,
+                        errors.ClientErrorTimedOut,
+                        errors.ServerError]
+        test_message = "Test additional info."
+        for cls in test_classes:
+            with self.subTest(cls):
+                err = cls(additional_info=test_message)
+                self.assertIn(test_message, err.error_message)
