1bb18f508adbce8155788ce77edf1cfb9890ebb3
bgrant <robert.grant@salesforce.com>
2022-12-19 15:22:16 -0500

    release: Release 0.25.0 (#115)
    * Bump the versin
    
    * Update the CHANGELOG

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,7 +74,7 @@ author = 'Salesforce'
 # built documents.
 
 # The full version, including alpha/beta/rc tags.
-release = '0.24.0'  # set by bumpversion
+release = '0.25.0'  # set by bumpversion
 
 # The short X.Y version.
 version = release.rsplit('.', 1)[0]
